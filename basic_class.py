5020ca83c8de2578d9c0e4dba4d9ab13bf0a4883
Harsha Vardhan <115510203+drowsyweirdo@users.noreply.github.com>
2024-07-01 15:06:41 -0500

    Add files via upload

---
+++ basic_class.py
@@ -0,0 +1,35 @@
+#variables 
+
+# names cannot start with a number
+# there should be no spaces in the name use "_" instead
+# cannot use most of the special characters
+
+#strings are immutable in python
+
+# print formatting with strings
+
+# print('This is a string {}'.foramt('INSERTED'))
+# o/p This is a string INSERTED
+
+# print('The {0} {0} {0}'.format('fox','brown','quick'))
+
+# print('The {q} {b} {f}'.format(f='fox',b='brown',q='quick'))
+
+# name = "Harsha"
+# print(f'hello my name is {name}')
+
+
+#dict 
+
+# my_dict = {'key1' : 'value1' , 'key2' : 'value2'}
+# print(my_dict)
+
+# prices = {'apple':2.99,'oranges' : 1.99,'milk': 5.80}
+# print(prices['apple'])
+
+
+# name = 'Mississippi'
+# name.split()
+# print(set(name))
+
+# print(set(list('Mississippi')))
